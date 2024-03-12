75cb419c8c72a86216f1d837edee6afbf9976c9c
SametcanOcak <ocaksametcan@gmail.com>
2024-03-12 15:57:07 +0300

    Fotoğrafçı Sitesi

---
+++ fotografci/index.js
@@ -0,0 +1,3 @@
+gsap.from('header, h2, p, #aboutpic', 1.2, {opacity:0, y:-80, delay:0.5})
+gsap.from('#carouselExampleIndicators', 1.4, {opacity:0, y:-80, delay:0.8})
+gsap.from('#text', 2.5, {opacity:0, y:-80, delay:0.8})
